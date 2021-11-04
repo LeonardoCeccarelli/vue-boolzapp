7feeac51cd0c8b64b68d7d759ef9c0c7a3fb5698
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2021-11-04 12:34:33 +0100

    try add  event click for delete message

---
+++ milestone_5/script.js
@@ -0,0 +1,164 @@
+"use strict"
+
+Vue.config.devtools = true;
+
+new Vue({
+    el: "#root",
+    data: {
+        contacts: [
+            {
+                name: 'Michele',
+                avatar: '_1',
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        text: 'Ricordati di dargli da mangiare',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        text: 'Tutto fatto!',
+                        status: 'received'
+                    }
+                ],
+            },
+            {
+                name: 'Fabio',
+                avatar: '_2',
+                messages: [
+                    {
+                        date: '20/03/2020 16:30:00',
+                        text: 'Ciao come stai?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '20/03/2020 16:30:55',
+                        text: 'Bene grazie! Stasera ci vediamo?',
+                        status: 'received'
+                    },
+                    {
+                        date: '20/03/2020 16:35:00',
+                        text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                        status: 'sent'
+                    }
+                ],
+            },
+            {
+                name: 'Samuele',
+                avatar: '_3',
+                messages: [
+                    {
+                        date: '28/03/2020 10:10:40',
+                        text: 'La Marianna va in campagna',
+                        status: 'received'
+                    },
+                    {
+                        date: '28/03/2020 10:20:10',
+                        text: 'Sicuro di non aver sbagliato chat?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '28/03/2020 16:15:22',
+                        text: 'Ah scusa!',
+                        status: 'received'
+                    }
+                ],
+            },
+            {
+                name: 'Luisa',
+                avatar: '_4',
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Lo sai che ha aperto una nuova pizzeria?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        text: 'Si, ma preferirei andare al cinema',
+                        status: 'received'
+                    }
+                ],
+            },
+        ],
+
+        currentIndexConversation: 0,
+
+        newMessageToAdd: "",
+
+        filteredContact: ""
+
+    },
+    methods: {
+        onChangeCurrentConversation(index) {
+            return this.currentIndexConversation = index
+        },
+
+        onSentNewMessage() {
+
+            if (this.newMessageToAdd.trim() === "") {
+                return
+            }
+
+            this.contacts[this.currentIndexConversation].messages.push({
+                date: '10/01/2020 15:50:00',
+                text: this.newMessageToAdd,
+                status: 'sent'
+            })
+
+            this.newMessageToAdd = ""
+
+            setTimeout(this.responseFromComputer, 1000)
+        },
+
+        responseFromComputer() {
+
+            this.contacts[this.currentIndexConversation].messages.push({
+                date: '10/01/2020 15:50:00',
+                text: 'Ok!',
+                status: 'recived'
+            })
+        },
+
+        getFilteredContacts() {
+            let newArray = this.contacts.filter((el) => {
+                let filterValue = true
+
+                if (el.name.toLowerCase().includes(this.filteredContact.toLowerCase())) {
+                    filterValue = true
+                } else {
+                    filterValue = false
+                }
+
+                return filterValue
+            })
+
+            return newArray
+        },
+
+        getLastMessage(messages) {
+
+            if (messages.length === 0) {
+                return ""
+            }
+
+            const lastMessage = messages[messages.length - 1].text
+
+            if (lastMessage.length > 30) {
+                return lastMessage.slice(0, 30) + " ..."
+            } else {
+                return lastMessage
+            }
+        },
+
+        onClickRemoveMessage(index, currentConversation) {
+
+            return this.contacts[currentConversation].messages.splice(index, 1)
+        }
+    }
+})
